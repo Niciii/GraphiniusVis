7a283555f7847eea80e3696c91b65e9855da8562
Nicole Neuhold <nicole.neuhold@student.tugraz.at>
2016-02-17 15:47:11 +0100

    visualisation with buffergeometry - works very well :)

--- src/js/graphThree.js
+++ src/js/graphThree.js
@@ -19,121 +19,107 @@ var ZOOM_FACTOR = 0.05,
 
 renderButton.onclick = function() {
   initGraph();
-  window.$GV.render();
+  window.$GV.renderGraph();
 }
 
-function render() {
+function renderGraph() {
   var MIN_X = MAX_X = nodes_obj[0].getFeature('coords').x,
       MIN_Y = MAX_Y = nodes_obj[0].getFeature('coords').y,
       MIN_Z = MAX_Z = nodes_obj[0].getFeature('coords').z;
 
   var scene = new THREE.Scene(); // Create a Three.js scene object.
-  var camera = new THREE.PerspectiveCamera(70, WIDTH / HEIGHT, 0.1, 1000); // Define the perspective camera's attributes.
-
-  var renderer = window.WebGLRenderingContext ? new THREE.WebGLRenderer() : new THREE.CanvasRenderer(); // Fallback to canvas renderer, if necessary.
+  var camera = new THREE.PerspectiveCamera(70, WIDTH / HEIGHT, 0.1, 1000); //TODO change to 0.1// Define the perspective camera's attributes.
+  var renderer = new THREE.WebGLRenderer({ antialias: false }); //window.WebGLRenderingContext ? new THREE.WebGLRenderer() : new THREE.CanvasRenderer(); // Fallback to canvas renderer, if necessary.
   renderer.setSize(WIDTH, HEIGHT); // Set the size of the WebGL viewport.
-  //document.body.appendChild(renderer.domElement); // Append the WebGL viewport to the DOM.
+  renderer.setClearColor( 0x000000, 1 );
+  //renderer.setPixelRatio( window.devicePixelRatio );
+  //renderer.gammaInput = true;
+	//renderer.gammaOutput = true;
   var element = document.getElementById("containerGraph");
   element.appendChild(renderer.domElement); // Append the WebGL viewport to the DOM.
-
-  var edges = new THREE.Group();
-  var nodes = new THREE.Group();
-  window.network = new THREE.Group();
-
-  // Be aware that a light source is required for MeshPhongMaterial to work:
-  var pointLight = new THREE.PointLight(0xFFFFFF); // Set the color of the light source (white).
-  pointLight.position.set(100, 100, 250); // Position the light source at (x, y, z).
-  scene.add(pointLight); // Add the light source to the scene.
-
-  //NODE
-  //var texture = THREE.ImageUtils.loadTexture(bitmap.src); // Create texture object based on the given bitmap path.
-  //material = new THREE.MeshBasicMaterial({ color: 0xFF00FF, wireframe: true });
-  //new THREE.MeshLambertMaterial ({color: 0xCC0000});
-  var material = new THREE.MeshPhongMaterial({ color: 0x1f9ed5/*0xCC0000*/ }); // Create a material (for the spherical mesh) that reflects light, potentially causing sphere surface shadows.
-  var geometry = new THREE.SphereGeometry(5, 64, 64); // Radius size, number of vertical segments, number of horizontal rings.
-
-  for(node in nodes_obj) {
-    var sphere = new THREE.Mesh(geometry, material);
-    var x = nodes_obj[node].getFeature('coords').x,
-        y = nodes_obj[node].getFeature('coords').y,
-        z = nodes_obj[node].getFeature('coords').z;
-
+  
+  var i = 0;
+  var vertices = new Float32Array( graph.nrNodes() * 3 );
+  for(node in node_keys) {
+    var x = nodes_obj[node].getFeature('coords').x;
+    var y = nodes_obj[node].getFeature('coords').y;
+    var z = nodes_obj[node].getFeature('coords').z;
+    
+    vertices[ i*3 ] = x;
+    vertices[ i*3 + 1 ] = y;
+    vertices[ i*3 + 2 ] = z;
+    i++;
+    
     MIN_X = Math.min(MIN_X, x);
     MIN_Y = Math.min(MIN_Y, y);
     MIN_Z = Math.min(MIN_Z, z);
-
+    
     MAX_X = Math.max(MAX_X, x);
     MAX_Y = Math.max(MAX_Y, y);
     MAX_Z = Math.max(MAX_Z, z);
-
-    sphere.position.set( x, y, z );
-    nodes.add(sphere);
   }
-  network.add(nodes);
+
+  var geometry = new THREE.BufferGeometry();    
+  geometry.addAttribute('position', new THREE.BufferAttribute(vertices, 3));
+  geometry.addAttribute('color', new THREE.BufferAttribute( 0x8aa7ff, 3));
+
+  var material = new THREE.PointsMaterial({ // MeshBasicMaterial
+    color: 0x8aa7ff,
+    size: 10
+    //map: texture
+  });  
+
+  particles = new THREE.Points( geometry, material );
+	//scene.add( particles );
 
   //EDGE
   var materialLine = new THREE.LineBasicMaterial({
-    color: 0x1f9ed5,
-    linewidth: 1
+    color: 0x81cf28
+   //vertexColors: THREE.VertexColors  //,
+    //linewidth: 1
+  });  
+
+  window.network = new THREE.Group();
+  network.add(particles);
+  
+  [und_edges, dir_edges].forEach(function(edges) {
+    var i = 0;
+    var positionLine = new Float32Array(Object.keys(edges).length * 6); //2 vertices * 3 xyz
+    for (var edge_index in edges) {
+      var edge = edges[edge_index];
+      var node_a_id = edge._node_a.getID();
+      var node_b_id = edge._node_b.getID();
+      
+      positionLine[ i * 6 ] = nodes_obj[node_a_id].getFeature('coords').x;
+      positionLine[ i * 6 + 1 ] = nodes_obj[node_a_id].getFeature('coords').y;
+      positionLine[ i * 6 + 2 ] = nodes_obj[node_a_id].getFeature('coords').z;
+      positionLine[ i * 6 + 3 ] = nodes_obj[node_b_id].getFeature('coords').x;
+      positionLine[ i * 6 + 4 ] = nodes_obj[node_b_id].getFeature('coords').y;
+      positionLine[ i * 6 + 5 ] = nodes_obj[node_b_id].getFeature('coords').z;
+      i++;
+    }
+    var geometryLine = new THREE.BufferGeometry();
+    geometryLine.addAttribute('position', new THREE.BufferAttribute(positionLine, 3));
+    //geometryLine.addAttribute('color', new THREE.BufferAttribute( 0xff700e, 3));
+    //geometry.computeBoundingSphere();
+    var line = new THREE.Line( geometryLine, materialLine );
+    //scene.add( line );
+    network.add(line);
   });
-
-  for (var u_edge in und_edges) {
-    edge = und_edges[u_edge];
-    node_a_id = edge._node_a.getID();
-    node_b_id = edge._node_b.getID();
-
-    var geometryLine = new THREE.Geometry();
-    geometryLine.vertices.push(
-      new THREE.Vector3(
-        nodes_obj[node_a_id].getFeature('coords').x,
-        nodes_obj[node_a_id].getFeature('coords').y,
-        nodes_obj[node_a_id].getFeature('coords').z
-      ),
-      new THREE.Vector3(
-        nodes_obj[node_b_id].getFeature('coords').x,
-        nodes_obj[node_b_id].getFeature('coords').y,
-        nodes_obj[node_b_id].getFeature('coords').z
-      )
-    );
-    var line = new THREE.Line( geometryLine, materialLine );
-    //scene.add( line );
-    edges.add(line);
-  }
-  for (var d_edge in dir_edges) {
-    edge = dir_edges[d_edge];
-    node_a_id = edge._node_a.getID();
-    node_b_id = edge._node_b.getID();
-
-    var geometryLine = new THREE.Geometry();
-    geometryLine.vertices.push(
-      new THREE.Vector3(
-        nodes_obj[node_a_id].getFeature('coords').x,
-        nodes_obj[node_a_id].getFeature('coords').y,
-        nodes_obj[node_a_id].getFeature('coords').z
-      ),
-      new THREE.Vector3(
-        nodes_obj[node_b_id].getFeature('coords').x,
-        nodes_obj[node_b_id].getFeature('coords').y,
-        nodes_obj[node_b_id].getFeature('coords').z
-      )
-    );
-    var line = new THREE.Line( geometryLine, materialLine );
-    //scene.add( line );
-    edges.add(line);
-  }
-  network.add(edges);
 
   network.translateX(-MAX_X/2);
   network.translateY(-MAX_Y/2);
   network.translateZ(-MAX_Z/2);
   scene.add(network);
   camera.position.z = Math.max(MAX_X, MAX_Y);
+  //camera.position.z = 500; // Move the camera away from the origin, down the positive z-axis.
+  //camera.lookat(new THREE.Vector3(0,0,0));
 
-  var render = function () {
+  var updateGraph = function () {
     renderer.render(scene, camera); // Each time we change the position of the cube object, we must re-render it.
     //requestAnimationFrame(render); // Call the render() function up to 60 times per second (i.e., up to 60 animation frames per second).
   };
-  window.requestAnimationFrame(render);
+  window.requestAnimationFrame(updateGraph);
 
   window.addEventListener('keypress', key, false);
   function key(event) {
@@ -142,17 +128,17 @@ function render() {
 
     switch (event.charCode) {
       case KEY_W: //zoom in
-        //camera.position.y = camera.position.y - deltaDistance; break;
-        network.translateY(deltaDistance); break;
+        camera.position.y = camera.position.y - deltaDistance; break;
+        //network.translateY(deltaDistance); break;
       case KEY_S: //zoom out
-        //camera.position.y = camera.position.y + deltaDistance; break;
-        network.translateY(-deltaDistance); break;
+        camera.position.y = camera.position.y + deltaDistance; break;
+        //network.translateY(-deltaDistance); break;
       case KEY_A: //move left
-        //camera.position.x = camera.position.x + deltaDistance; break;
-        network.translateX(-deltaDistance); break;
+        camera.position.x = camera.position.x + deltaDistance; break;
+        //network.translateX(-deltaDistance); break;
       case KEY_D: //move right
-        //camera.position.x = camera.position.x - deltaDistance; break;
-        network.translateX(deltaDistance); break;
+        camera.position.x = camera.position.x - deltaDistance; break;
+        //network.translateX(deltaDistance); break;
       case KEY_R:
         network.translateZ(deltaDistance); break;
       case KEY_F:
@@ -173,8 +159,7 @@ function render() {
         break;
     }
     //camera.updateProjectionMatrix();
-    window.requestAnimationFrame(render);
-    // render();
+    window.requestAnimationFrame(updateGraph);
   }
 
   window.addEventListener('mousewheel', mousewheel, false);
@@ -184,7 +169,7 @@ function render() {
     camera.fov -= ZOOM_FACTOR * event.wheelDeltaY;
     camera.fov = Math.max( Math.min( camera.fov, MAX_FOV ), MIN_FOV );
     camera.projectionMatrix = new THREE.Matrix4().makePerspective(camera.fov, WIDTH / HEIGHT, camera.near, camera.far);
-    window.requestAnimationFrame(render);
+    window.requestAnimationFrame(updateGraph);
   }
 
   window.addEventListener('click', mousedown, false);
@@ -205,7 +190,7 @@ function render() {
       intersects[ i ].object.material.color.set( 0xff0000 );
     }
     //console.log(nodes.children);
-    render();
+    updateGraph();
   }
 
   window.addEventListener('resize', windowResize, false);
@@ -227,6 +212,7 @@ function switchTo2D(network) {
     network.children[1].children[i].geometry.vertices[1].setZ(0);
     network.children[1].children[i].geometry.verticesNeedUpdate = true;
   }
+  window.requestAnimationFrame(updateGraph);
 }
 
 function switchTo3D() {
@@ -262,13 +248,14 @@ function switchTo3D() {
     network.children[1].children[i].geometry.verticesNeedUpdate = true;
     i++;
   }
+  window.requestAnimationFrame(updateGraph);
 }
 
 if (window !== 'undefined') {
   // window.test = test;
 
   window.$GV = {
-    render: render,
+    renderGraph: renderGraph,
     switchTo2D: switchTo2D,
     switchTo3D: switchTo3D
     // mutilateGraph: mutilateNGraph
